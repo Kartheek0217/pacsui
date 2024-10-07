2d7e4f43b255709963b5511d195acfaf20a5d9e4
Karthik Jakkula <50766097+Kartheek0217@users.noreply.github.com>
2024-10-07 22:49:05 +0530

    navigate

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ import { populateDropdown } from './utils/util.js';
 // Start the app when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', () => {
   const app = new App();
+  app.setupNavigation();
+  app.handleRouteChange();
   // Populate a dropdown with data from a REST API endpoint
   populateDropdown(
     'https://freetestapi.com/api/v1/airlines',
